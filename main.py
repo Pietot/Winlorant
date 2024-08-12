b086d87a85e44a9df6145ae4b74a34683887d9b7
VoltaniXx <voltanixf@gmail.com>
2024-08-12 02:10:24 +0200

    Added main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests as req
+import json
+
+API_KEY = "HDEV-284f2d01-91dd-40a7-b52b-5ae7dbbe1309"
+
+NAME = "Plouf VoltaniX"
+TAG = "0000"
+def createReq(url):
+    request = req.get(url, params={"api_key" : API_KEY})
+    gameJson = request.json()
+    
+
+    return gameJson
+
+gameJson = createReq(f"https://api.henrikdev.xyz/valorant/v1/stored-matches/eu/{NAME}/{TAG}")
+
+print(gameJson)
+
