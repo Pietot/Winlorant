b0b9b23b3c54c735f1ee01cbe805e945673788df
Pietot <102545552+Pietot@users.noreply.github.com>
2024-08-13 03:45:40 +0200

    Added type hinting, docstrings and get_day_by_date function

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""IDK"""
+
+from datetime import datetime
+from typing import Any
 import requests as req
 import json
 
@@ -5,14 +9,38 @@ API_KEY = "HDEV-284f2d01-91dd-40a7-b52b-5ae7dbbe1309"
 
 NAME = "Plouf VoltaniX"
 TAG = "9168"
-def createReq(url):
-    request = req.get(url, params={"api_key" : API_KEY})
-    gameJson = request.json()
-    
 
-    return gameJson
 
-gameJson = createReq(f"https://api.henrikdev.xyz/valorant/v1/stored-matches/eu/{NAME}/{TAG}")
+def get_request_json(url: str) -> dict[str, Any]:
+    """Get the json from the request
 
-for i in gameJson["data"]:
-    print(i["meta"]["map"]["name"])
+    Args:
+        url (str): The url of the request
+
+    Returns:
+        str: The json of the request
+    """
+    request: req.Response = req.get(url, params={"api_key": API_KEY}, timeout=30)
+    request_json: dict[str, Any] = dict(request.json())
+    return request_json
+
+
+def get_day_by_date(date_iso: str) -> str:
+    """Get the day by the date in iso format
+
+    Args:
+        date_iso (str): The date in iso format
+
+    Returns:
+        str: The day of the date
+    """
+    date_obj = datetime.fromisoformat(date_iso.replace("Z", "+00:00"))
+    day = date_obj.strftime("%A")
+    return day
+
+
+gameJson = get_request_json(f"https://api.henrikdev.xyz/valorant/v1/stored-matches/eu/{NAME}/{TAG}")
+
+for key in gameJson["data"]:
+    date = key["meta"]["started_at"]
+    print(get_day_by_date(date))
