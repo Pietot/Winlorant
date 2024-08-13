0b31f69b8cf454f54dfea18bf2dc1296734d56bb
VoltaniXx <voltanixf@gmail.com>
2024-08-13 03:05:05 +0200

    Added for loop

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import json
 API_KEY = "HDEV-284f2d01-91dd-40a7-b52b-5ae7dbbe1309"
 
 NAME = "Plouf VoltaniX"
-TAG = "0000"
+TAG = "9168"
 def createReq(url):
     request = req.get(url, params={"api_key" : API_KEY})
     gameJson = request.json()
@@ -14,5 +14,5 @@ def createReq(url):
 
 gameJson = createReq(f"https://api.henrikdev.xyz/valorant/v1/stored-matches/eu/{NAME}/{TAG}")
 
-print(gameJson)
-
+for i in gameJson["data"]:
+    print(i["meta"]["map"]["name"])
