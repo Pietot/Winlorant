c746e286e80d6b79ea9b9f90213132e11311b816
Pietot <102545552+Pietot@users.noreply.github.com>
2024-08-14 23:22:35 +0200

    Added range for the winrate

--- chart.php
+++ chart.php
@@ -14,6 +14,11 @@ function get_day_by_date($date_iso)
     return $day;
 }
 
+function get_day_by_timestamp($timestamp)
+{
+    return date('l', $timestamp);
+}
+
 function has_win(array $data): bool
 {
     $team = $data["stats"]["team"];
@@ -24,7 +29,7 @@ function has_win(array $data): bool
     return false;
 };
 
-function get_winrate_per_day(string $name, string $tag, string $oldest = "", string $newest = ""): array
+function get_winrate_per_day(string $name, string $tag, ?int $oldest = null, ?int $newest = null): array
 {
     $win_per_day = array(
         "Monday" => [0, 0],
@@ -39,10 +44,12 @@ function get_winrate_per_day(string $name, string $tag, string $oldest = "", str
     $game_json = get_request_json($name, $tag);
     foreach ($game_json["data"] as $key) {
         $date = $key["meta"]["started_at"];
-        if (($oldest !== "" && $date < $oldest) || ($newest !== "" && $date > $newest)) {
+        $date = strtotime($date);
+        if (($oldest !== null && $date < $oldest) || ($newest !== null && $date > $newest)) {
+            echo "nigger";
             continue;
         }
-        $day = get_day_by_date($date);
+        $day = get_day_by_timestamp($date);
         $win_per_day[$day][0] += has_win($key);
         $win_per_day[$day][1]++;
     }
