b480df2d2e02b5a742e2d673a217633265a6fc72
Pietot <102545552+Pietot@users.noreply.github.com>
2024-08-16 00:38:41 +0200

    Minor change

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Graphique en courbe</title>
+    <link rel="stylesheet" href="css/chart.css" />
+</head>
+
+<body>
+    <div class="chart-container">
+        <canvas id="myChart"></canvas>
+    </div>
+    <script src="js/chart.min.js"></script>
+    <script src="js/chart_configuration.js">
+    </script>
+</body>
+
+</html>
