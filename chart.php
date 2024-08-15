385b5bd931e55a9735e42b83f8d79049a0b074b1
VoltaniXx <voltanixf@gmail.com>
2024-08-15 03:10:39 +0200

    Creating the function get_number_game() and filtering game modes (unrated and competitive only)

--- chart.php
+++ chart.php
@@ -7,6 +7,24 @@ $tag = "0000";
 
 $game_json = get_request_json($name, $tag);
 
+
+
+// Competitive of unrated
+function get_number_game()
+{
+    global $game_json;
+    $nb_game = 0;
+    foreach ($game_json["data"] as $key) 
+    {
+        $mode = $key["meta"]["mode"];
+        if ($mode == "Competitive" || $mode == "Unrated") 
+        { 
+            $nb_game += 1;
+        }
+    }
+    return $nb_game;
+}
+
 function get_day_by_timestamp($timestamp)
 {
     $date_obj = new DateTime();
@@ -28,6 +46,7 @@ function has_win(array $data): bool
 function get_winrate_per_day(string $name, string $tag, ?int $oldest = null, ?int $newest = null): array
 {
     global $game_json;
+    
     $win_per_day = array(
         "Monday" => [0, 0],
         "Tuesday" => [0, 0],
@@ -40,11 +59,18 @@ function get_winrate_per_day(string $name, string $tag, ?int $oldest = null, ?in
 
     
     foreach ($game_json["data"] as $key) {
+        $mode = $key["meta"]["mode"];
+        if ($mode != "Competitive" && $mode != "Unrated") { 
+            continue; 
+        }
+
         $date = $key["meta"]["started_at"];
         $date = strtotime($date);
+
         if (($oldest !== null && $date < $oldest) || ($newest !== null && $date > $newest)) {
             continue;
         }
+
         $day = get_day_by_timestamp($date);
         $win_per_day[$day][0] += has_win($key);
         $win_per_day[$day][1]++;
@@ -53,12 +79,15 @@ function get_winrate_per_day(string $name, string $tag, ?int $oldest = null, ?in
     foreach ($win_per_day as $day => $value) {
         if ($value[1] === 0) {
             $win_per_day[$day] = "No games played";
-        } else {
+        } 
+        else {
             $win_per_day[$day] = round($value[0] / $value[1] * 100) . "%";
         }
     }
 
     return $win_per_day;
 }
+
+echo "<h2>" . get_number_game() . " parties ont été analysé. Ce nombre sera actualisé automatiquement tous les dimanches.</h2><br>";
 
 print_r(get_winrate_per_day($name, $tag));
