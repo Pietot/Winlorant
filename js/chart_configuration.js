599c5099b4c0194e7c94ad1289d836eb092a4bd7
Pietot <102545552+Pietot@users.noreply.github.com>
2024-08-17 01:59:54 +0200

    Added a special bar to show if a day has no data and maked hte padding under 0 only if one value in the data is lower than 0.1

--- js/chart_configuration.js
+++ js/chart_configuration.js
@@ -1,3 +1,48 @@
+function verifyLowestWinrate(data) {
+  for (let element of data) {
+    if (element < 0.1) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function addOffset(data) {
+  needPadding = true;
+  data.forEach((element, index) => {
+    if (element !== true) {
+      data[index] = element + offset;
+    }
+  });
+  return data;
+}
+
+function getData() {
+  return [true, 0.01, 0.3, 0.4, 0.5, 0.6, 1];
+}
+
+function drawZebraStripes(context, strokeStyle) {
+  const canvas = document.createElement("canvas");
+  const size = 50;
+  canvas.width = size;
+  canvas.height = size;
+  const ctx = canvas.getContext("2d");
+
+  ctx.fillStyle = "rgba(0, 0, 0, 0)";
+  ctx.fillRect(0, 0, size, size);
+  ctx.strokeStyle = strokeStyle;
+  ctx.lineWidth = 2;
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  ctx.lineTo(size, size);
+  ctx.moveTo(-size / 2, size / 2);
+  ctx.lineTo(size / 2, size + size / 2);
+  ctx.moveTo(size / 2, -size / 2);
+  ctx.lineTo(size + size / 2, size / 2);
+  ctx.stroke();
+  return context.chart.ctx.createPattern(canvas, "repeat");
+}
+
 const COLORS = [
   "rgb(255, 0, 0)",
   "rgb(255, 77, 0)",
@@ -11,8 +56,20 @@ const COLORS = [
   "rgb(0, 255, 179)",
   "rgb(0, 255, 255)",
 ];
+
+let offset = 0;
+let needPadding = false;
 const ctx = document.getElementById("myChart").getContext("2d");
-const OFFSET = 0.1;
+const DATA = (function () {
+  let data = getData();
+  if (verifyLowestWinrate(data)) {
+    offset = 0.1;
+    needPadding = true;
+    return addOffset(data);
+  }
+  console.log(data);
+  return data;
+})();
 
 const myChart = new Chart(ctx, {
   type: "bar",
@@ -28,40 +85,49 @@ const myChart = new Chart(ctx, {
     ],
     datasets: [
       {
-        data: [null, 0.2, 0.3, 0.4, 0.5, 0.6, 1.1],
+        data: DATA,
         backgroundColor: function (context) {
           let value =
-            context.raw !== null
-              ? Math.round((context.raw - OFFSET) * 100) / 100
-              : null;
+            context.raw === true
+              ? true
+              : Math.round((context.raw - offset) * 100) / 100;
           const alpha = 0.4;
 
-          if (value === null) {
-            return `rgba(128, 128, 128, ${alpha});`;
+          if (value === true) {
+            return drawZebraStripes(context, "rgb(150, 150, 150)");
           }
-
-          let index = Math.floor((value) * 10);
+          let index = Math.floor(value * 10);
           let rgb = COLORS[index].match(/\d+/g);
           return `rgba(${rgb[0]}, ${rgb[1]}, ${rgb[2]}, ${alpha})`;
         },
         borderColor: function (context) {
           let value =
-            context.raw !== null
-              ? Math.round((context.raw - OFFSET) * 100) / 100
-              : null;
-          const alpha = 1;
+            context.raw === true
+              ? true
+              : Math.round((context.raw - offset) * 100) / 100;
 
-          if (value === null) {
-            return `rgba(128, 128, 128, ${alpha});`;
+          if (value === true) {
+            return "rgb(150, 150, 150)";
           }
-          let index = Math.floor((value) * 10);
+          let index = Math.floor(value * 10);
           return COLORS[index];
         },
         borderWidth: 3,
         hoverBackgroundColor: function (context) {
+          if (context.raw === true) {
+            return drawZebraStripes(context, "rgb(100, 100, 100)");
+          }
           return context.dataset.borderColor(context);
         },
-        hoverBorderWidth: 0,
+        hoverBorderColor: function (context) {
+          if (context.raw === true) {
+            return "rgb(100, 100, 100)";
+          }
+          return context.dataset.borderColor(context);
+        },
+        hoverBorderWidth: function (context) {
+          return context.raw === true ? 3 : 0;
+        },
       },
     ],
   },
@@ -85,14 +151,16 @@ const myChart = new Chart(ctx, {
       },
       y: {
         beginAtZero: true,
-        max: 1.1,
+        max: function () {
+          return needPadding ? 1 + offset : 1;
+        },
         min: 0,
         ticks: {
           callback: function (value) {
-            if (value === 0) {
+            if (value === 0 && needPadding) {
               return "";
             }
-            return Math.round((value - 0.1) * 100) + "%";
+            return Math.round((value - offset) * 100) + "%";
           },
           stepSize: 0.1,
           color: "rgba(255, 255, 255, 0.7)",
@@ -112,14 +180,13 @@ const myChart = new Chart(ctx, {
       tooltip: {
         callbacks: {
           label: function (context) {
-            if (context.raw === null) {
-              return "No game played yet this day";
-            }
             let value =
-              context.raw !== null
-                ? Math.round((context.raw - OFFSET) * 100) / 100
-                : null;
-            return " Winrate: " + Math.round(value * 100) + "%";
+              context.raw === true
+                ? true
+                : Math.round((context.raw - offset) * 100);
+            return value !== true
+              ? " Winrate: " + Math.round(value) + "%"
+              : " No match played for this day.";
           },
         },
       },
