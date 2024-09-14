c83856d02912ddaa191dad3a03da1755a3b0e3f1
Pietot <102545552+Pietot@users.noreply.github.com>
2024-09-14 21:07:58 +0200

    Removed is_competitive and is_unrated functions

--- public/php/data_functions.php
+++ public/php/data_functions.php
@@ -1,23 +1,4 @@
 <?php
-
-function is_competitive(array $data): bool
-{
-    $mode = $data["meta"]["mode"];
-    if ($mode === "Competitive") {
-        return true;
-    }
-    return false;
-}
-
-function is_unrated(array $data): bool
-{
-    $mode = $data["meta"]["mode"];
-    if ($mode === "Unrated") {
-        return true;
-    }
-    return false;
-}
-
 function get_day_by_timestamp($timestamp)
 {
     $date_obj = new DateTime();
