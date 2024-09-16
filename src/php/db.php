2daba8ee43f2de2203c2c5d56a94dedd80b0a57b
Pietot <102545552+Pietot@users.noreply.github.com>
2024-09-16 21:04:24 +0200

    Added db.php (connection to database)

---
+++ src/php/db.php
@@ -0,0 +1,9 @@
+<?php
+$database_config = include_once '../src/config/database.php';
+
+$host = $database_config['host'];
+$dbname = $database_config['dbname'];
+$user = $database_config['user'];
+$password = $database_config['password'];
+
+$db = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
