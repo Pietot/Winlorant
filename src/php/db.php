67c588aa6c96902f99f31c1b7681bdfaec20349d
Pietot <102545552+Pietot@users.noreply.github.com>
2024-09-21 22:15:24 +0200

    Added

--- src/php/db.php
+++ src/php/db.php
@@ -5,5 +5,6 @@ $host = $database_config['host'];
 $dbname = $database_config['dbname'];
 $user = $database_config['user'];
 $password = $database_config['password'];
+$api_key = $database_config['api_key'];
 
 $db = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
