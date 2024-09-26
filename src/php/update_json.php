e10d3c232cde6daaed769abcc0b6402b5aff525c
Pietot <102545552+Pietot@users.noreply.github.com>
2024-09-26 20:52:07 +0200

    Finished update_json.php

--- src/php/update_json.php
+++ src/php/update_json.php
@@ -18,10 +18,28 @@ function update_json(): void
     $users = get_users();
     for ($i = 0; $i < count($users); $i++) {
         $user = $users[$i];
+        $id = $user['id'];
         $username = $user['username'];
         $tag = $user['tag'];
         $region = $user['region'];
         $user_json = get_data_json($username, $tag, $region);
+        if (isset($user_json['errors'])) {
+            if ($user_json['errors'][0]['message'] === "Rate limited") {
+                $message = "Rate limit exceeded" . "Time: " . date("Y-m-d H:i:s") . "\n\n";
+                file_put_contents(__DIR__ . "../../logs/logs.txt", $message, FILE_APPEND);
+                sleep(10);
+                $i--;
+            } else {
+                $message = "Error on user $username (id: $id):\n" . "Message:" . $user_json['errors'][0]['message'] . "\nDetails:" . $user_json['errors'][0]['details'] . "\nTime: " . date("Y-m-d H:i:s") . "\n\n";
+                file_put_contents(__DIR__ . "/../../logs/logs.txt", $message, FILE_APPEND);
+            }
+        } else {
+            $json = json_encode($user_json, 0);
+            file_put_contents(__DIR__ . "/../json/$id.json.gz", gzencode($json));
+            compress($id);
+            $message = "User $username (id: $id) updated successfully" . " Time: " . date("Y-m-d H:i:s") . "\n";
+            file_put_contents(__DIR__ . "/../../logs/logs.txt", $message, FILE_APPEND);
+        }
     }
 }
 
